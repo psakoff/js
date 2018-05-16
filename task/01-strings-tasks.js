a06de63f34c34440ae2b80a7ada7b438731b45ae
vsakoff <32842376+vsakoff@users.noreply.github.com>
2018-05-17 01:38:38 +0300

    Add files via upload

---
+++ task/01-strings-tasks.js
@@ -0,0 +1,311 @@
+'use strict';
+
+/********************************************************************************************
+ *                                                                                          *
+ * Перед началом работы с таском, пожалуйста ознакомьтесь с туториалом:                     *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String  *
+ *                                                                                          *
+ ********************************************************************************************/
+
+
+
+/**
+ * Возвращает результат конкатенации двух строк
+ *
+ * @param {string} value1
+ * @param {string} value2
+ * @return {string}
+ *
+ * @example
+ *   'aa', 'bb' => 'aabb'
+ *   'aa',''    => 'aa'
+ *   '',  'bb'  => 'bb'
+ */
+function concatenateStrings(value1, value2) {
+    //throw new Error('Not implemented');
+    return value1+value2;
+}
+
+
+/**
+ * Возвращает длину строки
+ *
+ * @param {string} value
+ * @return {number}
+ *
+ * @example
+ *   'aaaaa' => 5
+ *   'b'     => 1
+ *   ''      => 0
+ */
+function getStringLength(value) {
+    return value.length;
+}
+
+/**
+ * Возвращает строку образованную на основе шаблона и входных параметров firstName и lastName.
+ * Важно! Вместо использования конкатенации, используйте шаблоны :
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/template_strings
+ *
+ * @param {string} firstName
+ * @param {string} lastName
+ * @return {string}
+ *
+ * @example
+ *   'John','Doe'      => 'Hello, John Doe!'
+ *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
+ */
+function getStringFromTemplate(firstName, lastName) {
+    return 'Hello, ' + firstName + ' ' + lastName + '!';
+}
+
+/**
+ * Возвращает имя(First_Name) и фамилию(Last_Name) из шаблона 'Hello, First_Name Last_Name!'.
+ *
+ * @param {string} value
+ * @return {string}
+ *
+ * @example
+ *   'Hello, John Doe!' => 'John Doe'
+ *   'Hello, Chuck Norris!' => 'Chuck Norris'
+ */
+function extractNameFromTemplate(value) {
+  return value.slice(7,-1);
+}
+
+
+/**
+ * Возвращает первый символ полученной строки
+ *
+ * @param {string} value
+ * @return {string}
+ *
+ * @example
+ *   'John Doe'  => 'J'
+ *   'cat'       => 'c'
+ */
+function getFirstChar(value) {
+    return value[0];
+}
+
+/**
+ * Удаляет крайние левые и правые символы пробела или табуляции из строки.
+ *
+ * @param {string} value
+ * @return {string}
+ *
+ * @example
+ *   '  Abracadabra'    => 'Abracadabra'
+ *   'cat'              => 'cat'
+ *   '\tHello, World! ' => 'Hello, World!'
+ */
+function removeLeadingAndTrailingWhitespaces(value) {
+    return value.trim();
+}
+
+/**
+ * Возвращает заданную строку, повторяющуюся заданное число раз
+ *
+ * @param {string} value
+ * @param {string} count
+ * @return {string}
+ *
+ * @example
+ *   'A', 5  => 'AAAAA'
+ *   'cat', 3 => 'catcatcat'
+ */
+function repeatString(value, count) {
+    return value.repeat(count);
+}
+
+/**
+ * Удаляет первую встретившуюся последовательность симвоов из строки
+ *
+ * @param {string} str
+ * @param {string} value
+ * @return {string}
+ *
+ * @example
+ *   'To be or not to be', 'not'  => 'To be or to be'
+ *   'I like legends', 'end' => 'I like legs',
+ *   'ABABAB','BA' => 'ABAB'
+ */
+function removeFirstOccurrences(str, value) {
+  return str.replace(value, '');
+}
+
+/**
+ * Удаляет первую и последнюю угловую скобку в строке (тэге)
+ *
+ * @param {string} str
+ * @return {string}
+ *
+ * @example
+ *   '<div>' => 'div'
+ *   '<span>' => 'span'
+ *   '<a>' => 'a'
+ */
+//lollololo
+function unbracketTag(str) {
+  return str.slice(1,-1);
+}
+
+
+/**
+ * Переводит все символы в строке в верхний регистр
+ *
+ * @param {string} str
+ * @return {string}
+ *
+ * @example
+ *   'Thunderstruck' => 'THUNDERSTRUCK'
+ *  'abcdefghijklmnopqrstuvwxyz' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+ */
+function convertToUpperCase(str) {
+  return str.toUpperCase();
+}
+
+/**
+ * Из строки адресов электронной почты разделенных точкой с запятой, образует массив этих адресов
+ *
+ * @param {string} str
+ * @return {array}
+ *
+ * @example
+ *   'angus.young@gmail.com;brian.johnson@hotmail.com;bon.scott@yahoo.com' => ['angus.young@gmail.com', 'brian.johnson@hotmail.com', 'bon.scott@yahoo.com']
+ *   'info@gmail.com' => ['info@gmail.com']
+ */
+function extractEmails(str) {
+    return str.split(';');
+}
+
+/**
+ * Возвращает строковое предстваление фигуры с заданной шириной и высотой
+ * используя псевдографические символы: '┌', '─', '┐', '│', '└', '┘'.
+ *
+ * @param {number} width
+ * @param {number} height
+ * @return {string}
+ *
+ * @example
+ *
+ *            '┌────┐\n'+
+ *  (6,4) =>  '│    │\n'+
+ *            '│    │\n'+
+ *            '└────┘\n'
+ *
+ *  (2,2) =>  '┌┐\n'+
+ *            '└┘\n'
+ *
+ *             '┌──────────┐\n'+
+ *  (12,3) =>  '│          │\n'+
+ *             '└──────────┘\n'
+ *
+ */
+function getRectangleString(width, height) {
+    var str ='';
+    for (var i = 0 ; i <= height-1; i++){
+        if (i == 0)
+        {
+          str += '┌'+'─'.repeat(width-2) + '┐\n' ;
+        }
+        else if (i == height-1) {
+          str += '└'+'─'.repeat(width-2) + '┘\n' ;
+        }
+        else{
+          str += '│' + ' '.repeat(width-2) + '│\n';
+        }
+    }
+    return str;
+}
+
+
+/**
+ * Закодировать указанную строку при помощи ROT13 шифрования (*)
+ * See details:  https://en.wikipedia.org/wiki/ROT13
+ *
+ * @param {string} str
+ * @return {string}
+ *
+ * @example
+ *
+ *   'hello' => 'uryyb'
+ *   'Why did the chicken cross the road?' => 'Jul qvq gur puvpxra pebff gur ebnq?'
+ *   'Gb trg gb gur bgure fvqr!' => 'To get to the other side!'
+ *   'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz' => 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
+ *
+ */
+function encodeToRot13(str) {
+    var input = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    var output = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
+
+    var index = x => input.indexOf(x);
+    var translate = x => index(x) > -1 ? output[index(x)] : x;
+    return str.split('').map(translate).join('');
+}
+
+/**
+ * Возвращает булевое значение, является ли входной параметр строкой
+ * @param {string} value
+ * @return {boolean}
+ *
+ * @example
+ *   isString() => false
+ *   isString(null) => false
+ *   isString([]) => false
+ *   isString({}) => false
+ *   isString('test') => true
+ *   isString(new String('test')) => true
+ */
+function isString(value) {
+  return toString.call(value) == '[object String]';
+}
+
+
+/**
+ * Возвращает id игровой карты
+ *
+ * Исходная доска игровых карт представлена следующим порядком строк:
+ *
+ *  'A♣','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣',
+ *  'A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦',
+ *  'A♥','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥',
+ *  'A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠'
+ *
+ * (see https://en.wikipedia.org/wiki/Standard_52-card_deck)
+ * Function returns the zero-based index of specified card in the initial deck above.
+ *
+ * @param {string} value
+ * @return {number}
+ *
+ * @example
+ *   'A♣' => 0
+ *   '2♣' => 1
+ *   '3♣' => 2
+ *     ...
+ *   'Q♠' => 50
+ *   'K♠' => 51
+ */
+function getCardId(value) {
+  var cardset = "A♣,2♣,3♣,4♣,5♣,6♣,7♣,8♣,9♣,10♣,J♣,Q♣,K♣,A♦,2♦,3♦,4♦,5♦,6♦,7♦,8♦,9♦,10♦,J♦,Q♦,K♦,A♥,2♥,3♥,4♥,5♥,6♥,7♥,8♥,9♥,10♥,J♥,Q♥,K♥,A♠,2♠,3♠,4♠,5♠,6♠,7♠,8♠,9♠,10♠,J♠,Q♠,K♠".split(',');
+  return cardset.indexOf(value);
+}
+
+
+module.exports = {
+    concatenateStrings: concatenateStrings,
+    getStringLength: getStringLength,
+    getStringFromTemplate: getStringFromTemplate,
+    extractNameFromTemplate: extractNameFromTemplate,
+    getFirstChar: getFirstChar,
+    removeLeadingAndTrailingWhitespaces: removeLeadingAndTrailingWhitespaces,
+    repeatString: repeatString,
+    removeFirstOccurrences: removeFirstOccurrences,
+    unbracketTag: unbracketTag,
+    convertToUpperCase: convertToUpperCase,
+    extractEmails: extractEmails,
+    getRectangleString: getRectangleString,
+    encodeToRot13: encodeToRot13,
+    isString: isString,
+    getCardId: getCardId
+};
---
+++ task/06-conditions-n-loops-tasks.js
@@ -0,0 +1,637 @@
+'use strict';
+
+/**************************************************************************************************
+ *                                                                                                *
+ * Пожалуйста, прочтите информацию по ссылкам перед выполнением заданий:                                 *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Control_flow_and_error_handling  *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration              *
+ *                                                                                                *
+ **************************************************************************************************/
+
+
+/**
+ * Возврщает 'Fizz','Buzz' или начальное число согласно следеющим правилам:
+ * 1) если не подпадает под следйющте правила вернуть начальное число
+ * 2) число делится нацело на 3 вернуть 'Fizz'
+ * 3) число кратно 5 вернуть 'Buzz'
+ * 4) если число кратно 3 и 5 одновременно вернуть 'FizzBuzz'
+ *
+ * @param {number} num
+ * @return {any}
+ *
+ * @example
+ *   2 =>  2
+ *   3 => 'Fizz'
+ *   5 => 'Buzz'
+ *   4 => 4
+ *  15 => 'FizzBuzz'
+ *  20 => 'Buzz'
+ *  21 => 'Fizz'
+ *
+ */
+function getFizzBuzz(num) {
+
+
+  if(num % 3 == 0 && num % 5 == 0){
+    return 'FizzBuzz';
+  }
+  else if (num % 5 == 0) {
+    return 'Buzz';
+  }
+  else if (num % 3 == 0){
+    return 'Fizz';
+  }
+  else{
+    return num;
+  }
+
+}
+
+
+/**
+ * Возвращает факториал переданного целого числа n.
+ *
+ * @param {number} n
+ * @return {number}
+ *
+ * @example:
+ *   1  => 1
+ *   5  => 120
+ *   10 => 3628800
+ */
+function getFactorial(n) {
+  let res = 1;
+  for (let i = 1; i <= n ; i++)
+  {
+    res *= i;
+  }
+  return res;
+}
+
+
+/**
+ * Возвращается сумму целых чисел в промежутке между переданными числами, включая их
+ *
+ * @param {number} n1
+ * @param {number} n2
+ * @return {number}
+ *
+ * @example:
+ *   1,2   =>  3  ( = 1+2 )
+ *   5,10  =>  45 ( = 5+6+7+8+9+10 )
+ *   -1,1  =>  0  ( = -1 + 0 + 1 )
+ */
+function getSumBetweenNumbers(n1, n2) {
+  let sum = 0;
+  for (let i1 = n1; i1 <= n2; i1++)
+  {
+    sum+=i1;
+  }
+  return sum;
+    throw new Error('Not implemented');
+}
+
+
+/**
+ * Возвращает true, если с помощью трех переданных длин сторон a,b,c можно
+ * посроить треугольник, если нет - false
+ *
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {bool}
+ *
+ * @example:
+ *   1,2,3    =>  false
+ *   3,4,5    =>  true
+ *   10,1,1   =>  false
+ *   10,10,10 =>  true
+ */
+function isTriangle(a,b,c) {
+  if ((Math.abs(a-b) < c && c < Math.abs(a+b)) || (Math.abs(a-c) < b && b < Math.abs(a+c))|| (Math.abs(c-b) < a && a < Math.abs(c+b)))
+  {
+    return true;
+  }
+  return false;
+}
+
+
+/**
+ * Возвращает true, если 2 определенных прямоуголника перекрываются, если нет false.
+ * Каждый прямоуголник представлен обьектом
+ *  {
+ *     top: 5,
+ *     left: 5,
+ *     width: 20,
+ *     height: 10
+ *  }
+ *
+ *  (5;5)
+ *     -------------
+ *     |           |
+ *     |           |  height = 10
+ *     -------------
+ *        width=20
+ *
+ * NOTE: Пожлауйтса используйте принцип задания координат для canvas (https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Drawing_shapes#The_grid),
+ * этот способ отличается от декартовой системы координат.
+ *
+ * @param {object} rect1
+ * @param {object} rect2
+ * @return {bool}
+ *
+ * @example:
+ *   { top: 0, left: 0, width: 10, height: 10 },
+ *   { top: 5, left: 5, width: 20, height: 20 }    =>  true
+ *
+ *   { top: 0, left: 0, width: 10, height: 10 },
+ *   { top:20, left:20, width: 20, height: 20 }    =>  false
+ *
+ */
+function doRectanglesOverlap(rect1, rect2) {
+  return (rect1.top + rect1.height > rect2.top && rect1.left + rect1.width > rect2.left);
+}
+
+
+/**
+ * Возвращает true если точка лежим в пределах круга, если нет то false
+ * Круг представляет собой объект:
+ *  {
+ *     center: {
+ *       x: 5,
+ *       y: 5
+ *     },
+ *     radius: 20
+ *  }
+ *
+ * Точка представляет собой объект:
+ *  {
+ *     x: 5,
+ *     y: 5
+ *  }
+ *
+ * @param {object} circle
+ * @param {object} point
+ * @return {bool}
+ *
+ * @example:
+ *   { center: { x:0, y:0 }, radius:10 },  { x:0, y:0 }     => true
+ *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
+ *
+ */
+function isInsideCircle(circle, point) {
+  return Math.sqrt(Math.pow(circle.center.x - point.x, 2) + Math.pow(circle.center.y - point.y, 2)) < circle.radius;
+}
+
+
+/**
+ * Возврщает первый неповторяющийся символ в строке, если его нет то возвращает null.
+ *
+ * @param {string} str
+ * @return {string}
+ *
+ * @example:
+ *   'The quick brown fox jumps over the lazy dog' => 'T'
+ *   'abracadabra'  => 'c'
+ *   'entente' => null
+ */
+function findFirstSingleChar(str) {
+  for(let i = 0; i < str.length; i++)
+  {
+    let count = 1;
+    for (let j = 0; j < str.length; j++)
+    {
+      if (str[i]==str[j] && i!=j) count++;
+    }
+    if (count == 1 ) return str[i];
+  }
+  return null;
+    throw new Error('Not implemented');
+}
+
+
+/**
+ * Возвращает интервальную строку по 2 определенным числам и (включить / исключить) критериям.
+ * Подробное описание задачи: https://en.wikipedia.org/wiki/Interval_(mathematics)
+ *
+ * Обратите внимание на то, что меньшее число должно идти первым в описании
+ *
+ * @param {number} a
+ * @param {number} b
+ * @param {bool} isStartIncluded
+ * @param {bool} isEndIncluded
+ * @return {string}
+ *
+ * @example
+ *   0, 1, true, true   => '[0, 1]'
+ *   0, 1, true, false  => '[0, 1)'
+ *   0, 1, false, true  => '(0, 1]'
+ *   0, 1, false, false => '(0, 1)'
+ * меньшее число должно быть впереди :
+ *   5, 3, true, true   => '[3, 5]'
+ *
+ */
+function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
+    let str = '';
+    if (isStartIncluded){
+      str += '['
+    }
+    else{
+      str+= '('
+    }
+
+    if (a>=b){
+      str += b+', '+ a;
+
+    }
+    else{
+      str += a+', '+ b;
+    }
+    if (isEndIncluded){
+      str += ']'
+    }
+    else{
+      str+= ')'
+    }
+    return str;
+}
+
+
+/**
+ * Переворачивает переданную строку (ставит все символы строки в обратном порядке)
+ *
+ * @param {string} str
+ * @return {string}
+ *
+ * @example:
+ * 'The quick brown fox jumps over the lazy dog' => 'god yzal eht revo spmuj xof nworb kciuq ehT'
+ * 'abracadabra' => 'arbadacarba'
+ * 'rotator' => 'rotator'
+ * 'noon' => 'noon'
+ */
+function reverseString(str) {
+    return Array.from(str).reverse().join('');
+}
+
+
+/**
+ * Переворачивает переданное целое число (ставит все цифры числа в обратном порядке)
+ *
+ * @param {number} num
+ * @return {number}
+ *
+ * @example:
+ *   12345 => 54321
+ *   1111  => 1111
+ *   87354 => 45378
+ *   34143 => 34143
+ */
+function reverseInteger(num) {
+    return parseInt(Array.from(num.toString()).reverse().join(''));
+}
+
+
+/**
+ * Проверяет на валидность CCN (credit card number) и возвращает true если CCN валиден
+ * и возвращает false в противном случае.
+ *
+ * Описание алгоритма по ссылке : https://en.wikipedia.org/wiki/Luhn_algorithm
+ *
+ * @param {number} cnn
+ * @return {boolean}
+ *
+ * @example:
+ *   79927398713      => true
+ *   4012888888881881 => true
+ *   5123456789012346 => true
+ *   378282246310005  => true
+ *   371449635398431  => true
+ *
+ *   4571234567890111 => false
+ *   5436468789016589 => false
+ *   4916123456789012 => false
+ */
+function isCreditCardNumber(ccn) {
+
+  var str = '';
+    str += ccn;
+    var i;
+    var sum = Number(str[str.length - 1]);
+    var nDigits = str.length;
+    var parity = nDigits % 2;
+    for (i = 0; i < nDigits - 1; i++) {
+        var digit = Number(str[i])
+        if (i % 2 == parity)
+            digit = digit * 2;
+        if (digit > 9)
+            digit = digit - 9
+        sum += digit;
+    }
+    return sum % 10 == 0;
+}
+
+
+/**
+ * Возвращает сумму всех цифр переданного чила след. образом:
+ *   step1 : найти сумму всех цифр исходного числа
+ *   step2 : если сумма на step1 больше 9 нужно проделать step1 с полученной суммой
+ *
+ * @param {number} n
+ * @return {number}
+ *
+ * @example:
+ *   12345 ( 1+2+3+4+5 = 15, 1+5 = 6) => 6
+ *   23456 ( 2+3+4+5+6 = 20, 2+0 = 2) => 2
+ *   10000 ( 1+0+0+0+0 = 1 ) => 1
+ *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
+ */
+function getDigitalRoot(num) {
+  let n = num;
+  while(num > 9){
+     num = Array.from(num.toString()).reduce((sum,curr)=> sum+=parseInt(curr), 0);
+  }
+  return num;
+}
+
+
+/**
+ * Возвращает true если переданная строка представляет собой правильную скобочную
+ * структура, если нет -false
+ * Правильная скобочная структура состоит из соответствующих закрывающихся,
+ * открывающихся фигурных скобок, стоящих на соответствующих местях.
+ * Скобочная последовательность может содержать:  [],(),{},<>
+ *
+ * @param {string} str
+ * @return {boolean}
+ *
+ * @example:
+ *   '' => true
+ *   '[]'  => true
+ *   '{}'  => true
+ *   '()   => true
+ *   '[[]' => false
+ *   ']['  => false
+ *   '[[][][[]]]' => true
+ *   '[[][]][' => false
+ *   '{)' = false
+ *   '{[(<{[]}>)]}' = true
+ */
+function isBracketsBalanced(str) {
+    let arr = [];
+    let openbr = ['<','{','[','('];
+    let closebr = ['>','}',']',')'];
+
+    for (let i = 0; i < str.length ;i++)
+    {
+        if (openbr.indexOf(str[i]) != -1) arr.push(str[i]);
+
+        if (arr[arr.length-1] == '[' && str[i]==']') {arr.pop();}
+        else if(arr[arr.length-1] == '<' && str[i]=='>') {arr.pop();}
+        else if(arr[arr.length-1] == '{' && str[i]=='}') {arr.pop();}
+        else if(arr[arr.length-1] == '(' && str[i]==')') {arr.pop();}
+        else if (closebr.indexOf(str[i]) != -1)
+        {
+          arr.push(str[i]);
+        }
+    }
+
+    if (arr.length == 0){
+      return true;
+    }
+    return false;
+
+}
+
+
+/**
+ * Возвращает строку, составленной на основе периода от переданного начала и конца периода
+ * Конечная строка должна удовлетворять следующим правилам:
+ *
+ * ---------------------------------------------------------------------
+ *   Difference                 |  Result
+ * ---------------------------------------------------------------------
+ *    0 to 45 seconds           |  a few seconds ago
+ *   45 to 90 seconds           |  a minute ago
+ *   90 seconds to 45 minutes   |  2 minutes ago ... 45 minutes ago
+ *   45 to 90 minutes           |  an hour ago
+ *  90 minutes to 22 hours      |  2 hours ago ... 22 hours ago
+ *  22 to 36 hours              |  a day ago
+ *  36 hours to 25 days         |  2 days ago ... 25 days ago
+ *  25 to 45 days               |  a month ago
+ *  45 to 345 days              |  2 months ago ... 11 months ago
+ *  345 to 545 days (1.5 years) |  a year ago
+ *  546 days+                   |  2 years ago ... 20 years ago
+ * ---------------------------------------------------------------------
+ *
+ * @param {Date} startDate
+ * @param {Date} endDate
+ * @return {string}
+ *
+ * @example
+ *   Date('2000-01-01 01:00:00.100'), Date('2000-01-01 01:00:00.200')  => 'a few seconds ago'
+ *   Date('2000-01-01 01:00:00.100'), Date('2000-01-01 01:00:05.000')  => '5 minutes ago'
+ *   Date('2000-01-01 01:00:00.100'), Date('2000-01-02 03:00:05.000')  => 'a day ago'
+ *   Date('2000-01-01 01:00:00.100'), Date('2015-01-02 03:00:05.000')  => '15 years ago'
+ *
+ */
+function timespanToHumanString(startDate, endDate) {
+    let diffDate = new Date(endDate) - new Date(startDate) - 1/1000;
+
+    let day = diffDate/1000/60/60/24;
+    let hour = diffDate/1000/60/60;
+    let minute = diffDate/1000/60;
+    let secund = diffDate/1000;
+    if (day >= 545)
+       return (Math.round(day/365)) + " years ago";
+    else if (day >= 345)
+        return "a year ago";
+    else if (day >= 45)
+       return (Math.round(day/30)) + " months ago";
+    else if (day >= 25)
+       return "a month ago";
+    else if (hour >= 36)
+       return (Math.round(day)) + " days ago";
+    else if (hour >= 22)
+       return "a day ago";
+    else if (minute >= 90)
+       return (Math.round(hour)) + " hours ago";
+    else if (minute >= 45)
+       return "an hour ago";
+    else if (secund >= 90)
+       return (Math.round(minute)) + " minutes ago";
+    else if (secund >= 45)
+       return "a minute ago";
+    else return "a few seconds ago";
+}
+
+/**
+ * Вернуть строку с представление числа в n-ой (бинарной, десятичной, и т.д., где n<=10) системе исчисления.
+ * Более подробное описание
+ * https://en.wikipedia.org/wiki/Binary_number
+ * https://en.wikipedia.org/wiki/Ternary_numeral_system
+ * https://en.wikipedia.org/wiki/Radix
+ *
+ * @param {number} num
+ * @param {number} n, radix of the result
+ * @return {string}
+ *
+ * @example:
+ *   1024, 2  => '10000000000'
+ *   6561, 3  => '100000000'
+ *    365, 2  => '101101101'
+ *    365, 3  => '111112'
+ *    365, 4  => '11231'
+ *    365, 10 => '365'
+ */
+function toNaryString(num, n) {
+    let stack = [];
+    do{
+      stack.push(num % n);
+      num = parseInt(num / n);
+    }while(num>=n);
+    return num.toString() + stack.reverse().join('');
+}
+
+
+/**
+ * Возбращает общий путь к директории из всех путей переданных в массиве
+ *
+ * @param {array} pathes
+ * @return {string}
+ *
+ * @example:
+ *   ['/web/images/image1.png', '/web/images/image2.png']  => '/web/images/'
+ *   ['/web/assets/style.css', '/web/scripts/app.js',  'home/setting.conf'] => ''
+ *   ['/web/assets/style.css', '/.bin/mocha',  '/read.me'] => '/'
+ *   ['/web/favicon.ico', '/web-scripts/dump', '/webalizer/logs'] => '/'
+ */
+function getCommonDirectoryPath(pathes) {
+    for (let i = 0; i < pathes.length; i++){
+      pathes[i] = pathes[i].split('/');
+    }
+
+    let bool = true, count = 0, res = '';
+    while (bool)
+    {
+      let buffarr = [];
+      for (let i =0; i < pathes.length; i++){
+        buffarr.push(pathes[i][count]);
+      }
+      bool = buffarr.every((elem, index, array)=> elem == array[0]);
+      if (bool){
+        res+=buffarr[0]+'/';
+      }
+      count++;
+    }
+    return res;
+    throw new Error('Not implemented');
+}
+
+
+/**
+ * Возвращает произведение двух переданных матриц.
+ * Более подробное описание: https://en.wikipedia.org/wiki/Matrix_multiplication
+ *
+ * @param {array} m1
+ * @param {array} m2
+ * @return {array}
+ *
+ * @example:
+ *   [[ 1, 0, 0 ],       [[ 1, 2, 3 ],           [[ 1, 2, 3 ],
+ *    [ 0, 1, 0 ],   X    [ 4, 5, 6 ],     =>     [ 4, 5, 6 ],
+ *    [ 0, 0, 1 ]]        [ 7, 8, 9 ]]            [ 7, 8, 9 ]]
+ *
+ *                        [[ 4 ],
+ *   [[ 1, 2, 3]]    X     [ 5 ],          =>     [[ 32 ]]
+ *                         [ 6 ]]
+ *
+ */
+function getMatrixProduct(m1, m2) {
+
+  var m = m1.length, n = m2[0].length, l = m2.length;
+    var c = [];
+    for (var i = 0; i < m; i++){
+        c[i] = [];
+        for (var j = 0; j < n; j++){
+            c[i][j] = 0;
+        }
+    }
+
+    for (var i = 0; i < m; i++)
+        for (var j = 0; j < n; j++)
+            for(var k = 0; k < l; k++)
+                c[i][j] += m1[i][k] * m2[k][j];
+    return c;
+}
+
+
+/**
+ * Возвращает результат игры крестики-нолики для текущих позиций 'X', 'O'
+ * Более подробное описание: https://en.wikipedia.org/wiki/Tic-tac-toe
+ *
+ * Позиции X и O представлены в виде матрицы 3x3 cо значениями: 'X','0', undefined
+ * Функция должна возвращать победиля игры по текущей позиции.
+ * Результат должен быть в виде: 'X' или '0' или undefined
+ *
+ * @param {array} position
+ * @return {string}
+ *
+ * @example
+ *
+ *   [[ 'X',   ,'0' ],
+ *    [    ,'X','0' ],       =>  'X'
+ *    [    ,   ,'X' ]]
+ *
+ *   [[ '0','0','0' ],
+ *    [    ,'X',    ],       =>  '0'
+ *    [ 'X',   ,'X' ]]
+ *
+ *   [[ '0','X','0' ],
+ *    [    ,'X',    ],       =>  undefined
+ *    [ 'X','0','X' ]]
+ *
+ *   [[    ,   ,    ],
+ *    [    ,   ,    ],       =>  undefined
+ *    [    ,   ,    ]]
+ *
+ */
+function evaluateTicTacToePosition(position) {
+  if((position[0][0] == position[1][1]) && (position[0][0] == position[2][2]) && (position[1][1] == position[2][2]))
+       if(position[0][0])
+           return position[0][0];
+   if((position[0][2] == position[1][1]) && (position[0][2] == position[2][0]) && (position[1][1] == position[2][0]))
+       if(position[0][2])
+           return position[0][2];
+   for(let i = 0; i < 3; i++){
+       if((position[i][0] == position[i][1]) && (position[i][0] == position[i][2]) && (position[i][1] == position[i][2]))
+           if(position[i][0])
+               return position[i][1];
+       if ((position[0][i] == position[1][i]) && (position[0][i] == position[2][i]) && (position[1][i] == position[2][i]))
+           if(position[0][i])
+               return position[1][i];
+   }
+   return undefined;
+
+}
+
+
+module.exports = {
+    getFizzBuzz: getFizzBuzz,
+    getFactorial: getFactorial,
+    getSumBetweenNumbers: getSumBetweenNumbers,
+    isTriangle: isTriangle,
+    doRectanglesOverlap: doRectanglesOverlap,
+    isInsideCircle: isInsideCircle,
+    findFirstSingleChar: findFirstSingleChar,
+    getIntervalString : getIntervalString,
+    reverseString: reverseString,
+    reverseInteger: reverseInteger,
+    isCreditCardNumber: isCreditCardNumber,
+    getDigitalRoot: getDigitalRoot,
+    isBracketsBalanced: isBracketsBalanced,
+    timespanToHumanString : timespanToHumanString,
+    toNaryString: toNaryString,
+    getCommonDirectoryPath: getCommonDirectoryPath,
+    getMatrixProduct: getMatrixProduct,
+    evaluateTicTacToePosition : evaluateTicTacToePosition
+};
